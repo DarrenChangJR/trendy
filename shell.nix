8a1189b285395d18be0daef90db2b7693537ccd7
DarrenChangJR <darrenchangjr01@gmail.com>
2024-10-24 21:45:47 +0800

    Removed all unnecessary dependencies and added plotly

--- shell.nix
+++ shell.nix
@@ -5,16 +5,9 @@ in pkgs.mkShell {
   nativeBuildInputs = [
     pkgs.python311
     pkgs.python311Packages.torch-bin
-    pkgs.python311Packages.torchaudio-bin
-    pkgs.python311Packages.torch-audiomentations
-    pkgs.python311Packages.librosa
-    pkgs.python311Packages.jiwer
-    pkgs.python311Packages.datasets
-    pkgs.python311Packages.transformers
-    pkgs.python311Packages.evaluate
-    pkgs.python311Packages.accelerate
     pkgs.python311Packages.pip
-    
+    pkgs.python311Packages.pandas
+    pkgs.python311Packages.plotly
   ];
 
   shellHook = ''
